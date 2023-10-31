960e99efa0842e275b0f20e2c55e37abdcfcc343
tewlwolow <61018886+tewlwolow@users.noreply.github.com>
2023-10-31 09:25:44 +0100

    Uncomment all functionalities

--- MWSE/mods/telamur/main.lua
+++ MWSE/mods/telamur/main.lua
@@ -2,7 +2,7 @@ DRY_RUN = true
 
 event.register(tes3.event.initialized, function()
     if tes3.isModActive("Tel_Amur.esp") or DRY_RUN then
-        -- require("telamur.shaders.fogInterior")
+        require("telamur.shaders.fogInterior")
         require("telamur.music.controller")
         require("telamur.sounds.controller")
     end
