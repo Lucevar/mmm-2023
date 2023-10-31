bc309cc6abf2b45b5b25e6acf3a241bc0b5f53da
tewlwolow <61018886+tewlwolow@users.noreply.github.com>
2023-10-31 09:26:08 +0100

    Switch to water-based shader pos

--- MWSE/mods/telamur/shaders/fogInterior.lua
+++ MWSE/mods/telamur/shaders/fogInterior.lua
@@ -1,8 +1,12 @@
 local fog = require("telamur.shaders.fog")
+local cells = require("telamur.utils.cells")
 
 local fogId = "Tel Amur Interior"
 
----@type fogParams
+local isTelAmurFogCell = cells.isTelAmurFogCell
+
+DENSITY = 4
+
 local fogParams = {
     color = tes3vector3.new(0.78, 0.69, 0.21),
     center = tes3vector3.new(),
@@ -10,61 +14,54 @@ local fogParams = {
     density = 60,
 }
 
-local function calcInteriorFogParams(marker)
-    local root = tes3.game.worldObjectRoot
+-- Determine fog position for interiors --
+local function getFogLocation(cell)
+	local pos = { x = 0, y = 0, z = 0 }
+	local denom = 0
+	local xs, zs = {}, {}
 
-    local origin = root.worldBoundOrigin
-    local radius = root.worldBoundRadius
+	for stat in cell:iterateReferences() do
+		pos.x = pos.x + stat.position.x
+		pos.z = pos.z + stat.position.z
+		table.insert(xs, stat.position.x)
+		table.insert(zs, stat.position.z)
+		denom = denom + 1
+	end
 
-    fogParams.center.x = origin.x
-    fogParams.center.y = origin.y
-    fogParams.center.z = marker.position.z
-
-    fogParams.radius.x = radius
-    fogParams.radius.y = radius
-    fogParams.radius.z = math.deg(marker.orientation.z)
-
-    -- encoded such that 1.04 scale is 4 density
-    fogParams.density = (marker.scale - 1.0) * 100
+	return
+		{ x = pos.x / denom, y = pos.y / denom, z = 0 },
+		{
+			width = math.abs(math.max(table.unpack(xs)) - math.min(table.unpack(xs))),
+			depth =  math.abs(math.max(table.unpack(zs)) - math.min(table.unpack(zs))),
+		}
 end
 
-local markers = {}
-
-local function getActiveMarker()
-    local cell = tes3.player.cell
-    for marker in pairs(markers) do
-        if marker.cell == cell then
-            return marker
-        end
-    end
+local function calcInteriorFogParams(cell)
+    local fogPos, fogDim = getFogLocation(cell)
+    local depth = fogDim.depth
+    local width = fogDim.width
+    fogParams = {
+        color = tes3vector3.new(0.78, 0.69, 0.21),
+        center = tes3vector3.new(
+            fogPos.x,
+            fogPos.y,
+            fogPos.z
+		),
+        radius = tes3vector3.new(width, width, depth),
+        density = DENSITY
+    }
 end
+
 
 local function update()
-    local marker = getActiveMarker()
-    if marker then
-        calcInteriorFogParams(marker)
+    local cell = tes3.getPlayerCell()
+    local isAvailable = isTelAmurFogCell(cell)
+    if isAvailable then
+        calcInteriorFogParams(cell)
         fog.createOrUpdateFog(fogId, fogParams)
     else
         fog.deleteFog(fogId)
     end
 end
+
 event.register(tes3.event.cellChanged, update)
-
----@param e referenceActivatedEventData
-local function onReferenceCreated(e)
-    if e.reference.object.id == "ta_fog_layer" then
-        markers[e.reference] = true
-        update()
-    end
-end
-event.register(tes3.event.referenceActivated, onReferenceCreated)
-
----@param e referenceDeactivatedEventData|objectInvalidatedEventData
-local function onReferenceDeleted(e)
-    if markers[e.object or e.reference] then
-        markers[e.object or e.reference] = nil
-        update()
-    end
-end
-event.register(tes3.event.referenceDeactivated, onReferenceDeleted)
-event.register(tes3.event.objectInvalidated, onReferenceDeleted)
