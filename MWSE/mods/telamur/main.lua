ca06b994dfd17d996d66fc6eaa03c96e6159a7b0
tewlwolow <61018886+tewlwolow@users.noreply.github.com>
2023-10-31 09:33:08 +0100

    Change to latest `.esp` name

--- MWSE/mods/telamur/main.lua
+++ MWSE/mods/telamur/main.lua
@@ -1,7 +1,5 @@
-DRY_RUN = true
-
 event.register(tes3.event.initialized, function()
-    if tes3.isModActive("Tel_Amur.esp") or DRY_RUN then
+    if tes3.isModActive("Terror of Tel Amur.esp") then
         require("telamur.shaders.fogInterior")
         require("telamur.music.controller")
         require("telamur.sounds.controller")
