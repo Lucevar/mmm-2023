70371755ae4a71a0be28e1a7aacb2af7180a5f0c
tewlwolow <61018886+tewlwolow@users.noreply.github.com>
2023-11-03 20:01:11 +0100

    Add metadata and MCM

--- 01 MWSE addon/MWSE/mods/telamur/main.lua
+++ 01 MWSE addon/MWSE/mods/telamur/main.lua
@@ -1,7 +1,15 @@
-event.register(tes3.event.initialized, function()
-    if tes3.isModActive("Terror of Tel Amur.esp") then
-        require("telamur.shaders.fogInterior")
-        require("telamur.music.controller")
-        require("telamur.sounds.controller")
-    end
-end)
+if tes3.isModActive("Terror of Tel Amur.esp") then
+
+    event.register(tes3.event.initialized, function()
+            -- Loads our lua modules
+            require("telamur.shaders.fogInterior")
+            require("telamur.music.controller")
+            require("telamur.sounds.controller")
+    end)
+
+    -- Registers MCM menu
+    event.register(tes3.event.modConfigReady, function()
+        dofile("Data Files\\MWSE\\mods\\telamur\\mcm.lua")
+    end)
+
+end
